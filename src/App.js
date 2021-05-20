d323ffdb53ef71bfc56dc9292de0e1b23bc41cd4
Jobzi <jipsonmurillo@gmail.com>
2021-05-20 10:16:50 -0500

    add tailwind css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from './components/Card'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mx-auto'>
+    <h1>Tailwind CSS</h1>
+    <h2>Incorporado con PostCss</h2>
+    <button className="btn btn-blue btn-blue:hover">Botón</button>
+    <div className="my-5 flex flex-wrap -mx-2">
+        <div className="w-full sm:w-1/2 lg:w-1/3 px-2 my-2">
+            <Card photo='https://picsum.photos/800/600' />
+        </div>
+        <div className="w-full sm:w-1/2 lg:w-1/3 px-2 my-2">
+            <Card photo='https://picsum.photos/800/600' />
+        </div>
+        <div className="w-full sm:w-1/2 lg:w-1/3 px-2 my-2">
+            <Card photo='https://picsum.photos/800/600' />
+        </div>
     </div>
+</div>
   );
 }
 
