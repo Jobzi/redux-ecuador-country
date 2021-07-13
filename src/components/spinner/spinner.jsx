a4f30615cfba74662e783c086379a818cd5f7812
Jobzi <jipsonmurillo@gmail.com>
2021-07-13 12:22:25 -0500

    add spinne

---
+++ src/components/spinner/spinner.jsx
@@ -0,0 +1,8 @@
+import './spinner.css'
+export function Spinner () {
+  return (
+    <div className="spinner-content">
+        <div className="spinner"></div>
+    </div>
+  )
+}
--- src/pages/country/country.js
+++ src/pages/country/country.js
@@ -1,13 +1,25 @@
+import { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import { Spinner } from '../../components/spinner/spinner'
 import CardCountry from './card/card'
 import './country-style.css'
 
 function BodyComponent () {
   const countries = useSelector(state => state)
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    setLoading(false)
+  }, [countries])
   return (
-    <div className='ListOfCountries'>
-        {countries.map((country) => <CardCountry key={country.id} country={country}/>)}
-    </div>
+    <>
+    {
+      loading
+        ? <Spinner/>
+        : <div className='ListOfCountries'>
+            {countries.map((country) => <CardCountry key={country.id} country={country}/>)}
+        </div>
+    }
+    </>
   )
 }
 export default BodyComponent
