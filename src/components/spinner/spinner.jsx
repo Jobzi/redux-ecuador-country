814caea852f5a6cd339312ffc333f0e214463729
Jobzi <jipsonmurillo@gmail.com>
2021-07-22 18:31:07 -0500

    refactor sippner

--- src/components/spinner/spinner.jsx
+++ src/components/spinner/spinner.jsx
@@ -1,8 +1,13 @@
 import './spinner.css'
-export function Spinner () {
+function Spinner () {
   return (
     <div className="spinner-content">
         <div className="spinner"></div>
     </div>
   )
 }
+
+function OnlySpinner () {
+  return <div className="spinner"></div>
+}
+export { Spinner, OnlySpinner }
